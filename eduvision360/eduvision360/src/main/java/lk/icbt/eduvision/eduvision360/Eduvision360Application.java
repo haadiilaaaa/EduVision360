e11a8284994b3c424eb0e03fde1a07b6dcea71da
Hadi <hadilafassy@gmail.com>
2026-03-06 12:20:57 +0530

    chore(backend): add Spring Boot project scaffold, Maven wrapper, and base application files

---
+++ eduvision360/eduvision360/src/main/java/lk/icbt/eduvision/eduvision360/Eduvision360Application.java
@@ -0,0 +1,14 @@
+package lk.icbt.eduvision.eduvision360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Eduvision360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eduvision360Application.class, args);
+	}
+
+}
